9deb664a947eee432a1e9f006e10e0d400e3af91
thorkill <thkr@hannover.ccc.de>
2016-05-18 21:02:24 +0200

    Added .dir-locals.el for the emacs devs

---
+++ src/.dir-locals.el
@@ -0,0 +1,5 @@
+((nil . ((indent-tabs-mode . t)
+         (tab-width . 2)))
+ (c-mode . (
+            ;; Highlight leading space characters in Haskell files.
+            (eval . (highlight-regexp "^ *")))))
