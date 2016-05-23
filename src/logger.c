a429889bed6c70bb74c93a8f12d9cf4aa44a836f
thorkill <thkr@hannover.ccc.de>
2016-05-23 13:08:31 +0200

    Added replaywin into logger

--- src/logger.c
+++ src/logger.c
@@ -128,9 +128,9 @@ static void sptps_logger(sptps_t *s, int s_errno, const char *format, va_list ap
 			message[--len] = 0;
 
 		len += snprintf(message + len, sizeof message - len,
-										" [init:%d udp:%d state:%d instate:%d outstate:%d inseqno:%d outseqno:%d farfuture:%d] ", s->initiator, s->datagram,
+										" [init:%d udp:%d state:%d instate:%d outstate:%d inseqno:%d outseqno:%d farfuture:%d replaywin:%d] ", s->initiator, s->datagram,
 										s->state, s->instate, s->outstate, s->inseqno, s->outseqno,
-										s->farfuture);
+										s->farfuture, s->replaywin);
 
 		// WARNING: s->handle can point to a connection_t or a node_t,
 		// but both types have the name and hostname fields at the same offsets.
